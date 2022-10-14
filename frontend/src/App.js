67f10ee253ed72f73288e921f0c8abca94ec861c
Vincent Masse <vincentmasse@protonmail.com>
2022-10-14 14:08:33 +0200

    clean useless file + make it compile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
-import React from 'react'
 import {
   BrowserRouter as Router,
   Route,
   Switch
 } from 'react-router-dom'
-import './App.css'
-import Home from './Home'
+import Home from './Home.js'
 
 function App() {
   return (
-    <div className="App">
-      <Router >
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <>
+      <div className="App">
+        <Router >
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </>
   );
 }
 
-export default App;
+export default App
