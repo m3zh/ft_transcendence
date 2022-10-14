dfebb0d88b7c8d09b784b968d598c0d5536471ae
m3zh <mil3zh@protonmail.com>
2022-10-14 13:45:17 +0200

    frontend

---
+++ frontend/src/Home.js
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+        <>
+            <div className=" d-flex align-items-center justify-content-center">
+                Homepage
+            </div>
+        </>
+    )
+}
+
+export default Home
