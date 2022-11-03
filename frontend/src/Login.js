e1859fae653ea8c2f68bd5795af579ca5a83765a
m3zh <mlazzaretti@protonmail.com>
2022-11-03 15:54:23 +0100

    frontend: login

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -3,32 +3,21 @@ import React, { Component } from "react";
 export default class Login extends Component {
     render() {
         return (
-            <div>
-                <form>
+            <div >
+                <form class="d-grid gap-2">
 
-                    <h3>Log in</h3>
+                    <h3> 42transcendence </h3>
 
                     <div className="form-group">
-                        <label>Email</label>
-                        <input type="email" className="form-control" placeholder="Enter email" />
+                        <label>Login</label>
+                        <input type="email" className="form-control" placeholder="Enter your 42 login" />
                     </div>
 
                     <div className="form-group">
                         <label>Password</label>
                         <input type="password" className="form-control" placeholder="Enter password" />
                     </div>
-
-                    <div className="form-group">
-                        <div className="custom-control custom-checkbox">
-                            <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                            <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                        </div>
-                    </div>
-
-                    <button type="submit" className="btn btn-dark btn-lg btn-block">Sign in</button>
-                    <p className="forgot-password text-right">
-                        Forgot <a href="#">password?</a>
-                    </p>
+                    <button type="button" className="btn btn-dark btn-lg btn-block">Log in</button>
                 </form>
             </div>
         );
