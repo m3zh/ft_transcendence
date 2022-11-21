4ff23839fb488f392c7b657f65b73c988f300928
m3zh <mil3zh@protonmail.com>
2022-11-21 11:28:30 +0100

    42auth: api ok

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { Auth42Guard } from './guards'
 
-@Controller('auth')
+@Controller('auth') // this is default get url for this controller
 export class AuthController {
 
-    @Get('/auth') // this is a decorator, it states where the user will be redirected to authenticate
-                                     // do NOT add ; at the end of the line
+    @Get()  // this is a decorator, it states where the user will be redirected
+            // to authenticate
+            // do NOT add ; at the end of the line
+            // do NOT add / before the beginning of the path, eg. NO '/auth'
     @UseGuards(Auth42Guard)
     handleAuthentication(){
         return {
@@ -13,7 +15,7 @@ export class AuthController {
         };
     }
 
-    @Get('/home') // this has to math the route ( no need to write localhost:3001 ) redirection of the 42 api settings
+    @Get('home') // this has to math the route ( no need to write localhost:3001 ) redirection of the 42 api settings
     @UseGuards(Auth42Guard)
     handleRedirection(){
         return {
--- backend/src/auth/strategy42.service.ts
+++ backend/src/auth/strategy42.service.ts
@@ -6,10 +6,10 @@ import { Injectable } from '@nestjs/common';
 export class Strategy42 extends PassportStrategy(Strategy) {
     constructor() {
             super({
-                clientUID: process.env.CLIENT_UID,
+                clientID: process.env.CLIENT_ID,
                 clientSecret: process.env.CLIENT_SECRET,
-                callbackURL: 'http://localhost:3000/home',
-                scope: ['profile', 'email']
+                callbackURL: 'http://localhost:3001/home',
+                scope: ['public'],
             });
         }
 
