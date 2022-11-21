126c128c0d14ba6163f72b4f06c9ea9d4941c419
m3zh <mil3zh@protonmail.com>
2022-11-21 16:04:43 +0100

    42api: auth

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,13 +3,17 @@ import { Strategy42 } from './strategy42.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
   imports: [ PrismaModule ],
-  providers: [Strategy42, {
+  providers: [
+    Strategy42, {
     provide: 'AUTH_SERVICE',
     useClass: AuthService,
-  }],
+  },
+    UsersService
+  ],
   controllers: [AuthController]
 })
 export class AuthModule {}
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,23 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { UserData } from 'src/prisma/types';
-import { PrismaClient } from '@prisma/client';
 import { UsersService } from '../users/users.service'
 
 @Injectable()
 export class AuthService {
 
     constructor(
-       private readonly prisma : PrismaClient 
+        private readonly usersService: UsersService
     ) {}
 
 
     async validateUser(data: UserData){
         console.log('AuthService');
         console.log(data.uid);
-        const user = await this.prisma.users.findFirst({
-            where:
-                { uid: data.uid }
-        })
+        const user = await this.usersService.findOne(data.uid)
         if (user) return user;
 
     }
