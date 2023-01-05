422991490ef86d013c29055a23f2bea5a160c3ef
mil3zh <milena.lazzaretti5@gmail.com>
2023-01-05 18:56:44 +0100

    profile: edit avatar

--- frontend/src/EditProfile.jsx
+++ frontend/src/EditProfile.jsx
@@ -9,10 +9,11 @@ function EditProfile() {
     const user = useSelector((state) => state.userProvider.user);
     const src = useSelector((state) => state.userProvider.user.avatar);
     console.log(src)
-    const MAX_SIZE = 71680;
+    const MAX_SIZE = 711680;
     let [username, setUsername] = useState("");
     let [title, setTitle] = useState("");
     let [preview, setPreview] = useState(null);
+    console.log(preview)
 
     function onHandleUpdate(event) {
         event.preventDefault();
@@ -33,7 +34,7 @@ function EditProfile() {
 
       
     function onClose()          {    setPreview(null);       }
-    function onCrop(preview)    {    setPreview({preview});  }
+    function onCrop(preview)    {    setPreview(preview);  }
     function onBeforeFileLoad(elem) {
         if (elem.target.files[0].size > MAX_SIZE) {
           alert("File size too big!");
@@ -48,20 +49,41 @@ function EditProfile() {
                     <div className="col-md-12">
                         <div className="profile">
                             <div className="profile-header">
-                                <div className="profile-header-content">
-                                    <h4 className="m-t-10 m-b-5">Profile Information</h4>
-                                    <div className="profile-header-img">
-                                        <img style={{ height: "100%"}} className="img-thumbnail" src={ user.avatar } alt=""/>
+                                <div className="profile-header-content row align-items-start">
+                                    <h4 className="m-10">Profile Information</h4>
+                                    
+                                    <div className='col'> 
+                                        <div className="profile-header-img">
+                                            {
+                                                preview ?
+                                                <img style={{ height: "100%"}} className="img-thumbnail" src={ preview } alt="Preview"/>
+                                                :
+                                                <img style={{ height: "100%"}} className="img-thumbnail" src={ user.avatar } alt="Preview"/>
+                                            }
+                                        </div>
+                                        <div className="profile-header-info">
+                                            <form>
+                                                <label>Change your username</label><br></br>
+                                                <input style={{ placeholderTextColor: "gray" }} className="m-t-10 m-b-5" placeholder={ user.username } onChange={ (e)=> setUsername(e.target.value) }/><br></br>
+                                                <label>Wanna add a personal note?</label><br></br>
+                                                <input className="m-t-10 m-b-5" placeholder="ex: the best player ever" onChange={ (e)=> setTitle(e.target.value) }/><br></br>
+                                                <button type="submit" onClick={ (event) => onHandleUpdate(event) } className="btn btn-sm btn-warning mt-3">Update Profile</button>
+                                            </form> 
+                                        </div>
                                     </div>
-                                    <div className="profile-header-info">
-                                        <form>
-                                            <label>Change your username</label><br></br>
-                                            <input style={{ placeholderTextColor: "gray" }} className="m-t-10 m-b-5" placeholder={ user.username } onChange={ (e)=> setUsername(e.target.value) }/><br></br>
-                                            <label>Wanna add a personal note?</label><br></br>
-                                            <input className="m-t-10 m-b-5" placeholder="ex: the best player ever" onChange={ (e)=> setTitle(e.target.value) }/><br></br>
-                                            <button type="submit" onClick={ (event) => onHandleUpdate(event) } className="btn btn-sm btn-warning mt-2">Update Profile</button>
-                                        </form> 
+
+                                    <div className='col'>                                    
+                                        <Avatar
+                                            width={390}
+                                            height={295}
+                                            onCrop={onCrop}
+                                            onClose={onClose}
+                                            onBeforeFileLoad={onBeforeFileLoad}
+                                            src={src}
+                                            alt=""
+                                        />
                                     </div>
+
                                 </div>
                             </div>
                         </div>
