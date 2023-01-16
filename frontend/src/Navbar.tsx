71a39a7f68496486417b3e29f9f8dd7976414d64
m3zh <mil3zh@protonmail.com>
2023-01-16 18:02:28 +0100

    navbar: login/out

--- frontend/src/Navbar.tsx
+++ frontend/src/Navbar.tsx
@@ -35,12 +35,19 @@ function Navbar() {
                             <li className="nav-item">
                                 <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                             </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/pong">Play</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/chat">Chat</Link>
-                            </li>
+                            {
+                                loggedIn ?
+                                <>
+                                    <li className="nav-item">
+                                        <Link className="nav-link" to="/pong">Play</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link className="nav-link" to="/chat">Chat</Link>
+                                    </li>
+                                </>
+                                    :
+                                    <div></div>
+                            }
                         </ul>
                         
                         {
